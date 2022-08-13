cc1b50ee20c1073bb47fe34c013fa13c98096233
yeLounge <93738148+yeLounge@users.noreply.github.com>
2022-08-13 21:28:58 +0900

    .

--- dot/dottoconf.sh
+++ dot/dottoconf.sh
@@ -1,15 +1,16 @@
-cp Dropbox/dot/dot/dottoconf.sh ~
-cp Dropbox/dot/dot/conftodot.sh ~
-cp Dropbox/dot/dot/.xprofile ~
-cp Dropbox/dot/dot/.i3status.conf ~ 
-cp -r Dropbox/dot/dot/picom/ .config/
-cp -r Dropbox/dot/dot/alacritty/ .config/ 
-cp -r Dropbox/dot/dot/albert/ .config/ 
-cp -r Dropbox/dot/dot/fcitx/ .config/ 
-cp -r Dropbox/dot/dot/fish/ .config/ 
-cp -r Dropbox/dot/dot/i3/ .config/ 
-cp -r Dropbox/dot/dot/nvim/ .config/ 
-cp -r Dropbox/dot/dot/rofi/ .config/ 
-cp -r Dropbox/dot/dot/nitrogen/ .config/ 
-cp -r Dropbox/dot/dot/ranger/ .config/ 
+cp dot/dot/dottoconf.sh ~
+cp dot/dot/conftodot.sh ~
+cp dot/dot/.xprofile ~
+cp dot/dot/.i3status.conf ~ 
+cp -r dot/dot/picom/ .config/
+cp -r dot/dot/alacritty/ .config/ 
+cp -r dot/dot/albert/ .config/ 
+cp -r dot/dot/fcitx/ .config/ 
+cp -r dot/dot/fish/ .config/ 
+cp -r dot/dot/i3/ .config/ 
+cp -r dot/dot/nvim/ .config/ 
+cp -r dot/dot/rofi/ .config/ 
+cp -r dot/dot/nitrogen/ .config/ 
+cp -r dot/dot/ranger/ .config/ 
 
+sudo pacman -S  fish nautilus nitrogen smplayer rofi rofi-emoji unzip okular  fcitx fcitx-gtk2 fcitx-gtk3 fcitx-hangul fcitx-qt5 fcitx-table-extra fcitx-table-other kcm-fcitx adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts nerd-fonts-hack nerd-fonts-sf-mono noto-fonts noto-fonts-emoji-apple noto-fonts-extra ttf-dejavu ttf-joypixels
