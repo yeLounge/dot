db571b177c20bc32490c05cf3262cbe028e67123
yeLounge <2young0818@gmail.com>
2023-08-21 16:01:48 +0900

    .

--- dot/home/Script/boot.sh
+++ dot/home/Script/boot.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 i3-msg exec ~/Script/blue.sh
+i3-msg exec ~/Script/ws5.sh
 sleep 3
 i3-msg exec ~/Script/ws6.sh
-sleep 10
-i3-msg exec ~/Script/ws5.sh
-sleep 5
-i3-msg workspace 1
-xdotool mousemove 144 291
-xdotool click 1
-i3-msg workspace 2
-xdotool mousemove 144 291
-xdotool click 1
-i3-msg workspace 4
-xdotool mousemove 144 291
-xdotool click 1
-i3-msg workspace 5
-xdotool mousemove 144 291
-xdotool click 1
-xdotool mousemove 1181 81
-xdotool click 1
-xdotool mousemove 1223 533
-xdotool click 1
