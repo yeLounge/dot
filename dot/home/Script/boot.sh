a0f75c54989421b836250b80a573aa18a702c701
yeLounge <2young0818@gmail.com>
2023-08-24 23:22:21 +0900

    .

--- dot/home/Script/boot.sh
+++ dot/home/Script/boot.sh
@@ -1,17 +1,7 @@
-#!/bin/bash
-i3-msg exec ~/Script/blue.sh
-sleep 3
-i3-msg exec ~/Script/ws6.sh
-sleep 10 
-i3-msg exec ~/Script/ws5.sh
-sleep  
-i3-msg workspace 1
-i3-msg workspace 4
-i3-msg workspace 6
-xdotool mousemove 162 358
-xdotool mousemove 554 333
-xdotool mousemove 1059 339
-i3-msg workspace 5
-xdotool mousemove 144 291
-xdotool mousemove 1181 81
-xdotool mousemove 1223 533
+exec_always --no-startup-id pkill blueman-applet
+exec_always --no-startup-id ~/Script/blueboot.sh
+exec_always --no-startup-id ~/Script/day.sh
+exec_always --no-startup-id ~/Script/conftodot.sh
+exec_always --no-startup-id ~/Script/git.sh
+sleep 1 
+exec_always --no-startup-id ~/Script/ws.sh
