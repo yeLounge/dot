233d6475a4a5868dfe2b81e073a0638062142037
yeLounge <2young0818@gmail.com>
2023-08-26 16:20:53 +0900

    .

---
+++ dot/home/Script/foobar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+i3-msg [class="foobar2000"] focus
+xdotool mousemove 1057 287
+xdotool click 1
+sleep 0.1
+xdotool keyup --clearmodifiers "ctrl, alt, shift, super, l, return, a, v, c, p, s"
